1c4a56efdea7aed14d47cc29f509abddf4da9c12
Igor Napierala <igor.napierala@cognifide.com>
2021-01-08 20:56:14 +0100

    added prices component

---
+++ components/Prices.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Prices extends React.Component {
+  state = {
+    currency: "USD",
+  };
+  render() {
+    return <div>Prices Bro</div>;
+  }
+}
+
+export default Prices;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
+import Prices from "../components/Prices";
 
 var Index = (props) => (
   <Layout>
     <div>
       <h1>Welcome to Bits</h1>
-      {props.data.chartName}
+      <Prices bpi={props.bpi} />
     </div>
   </Layout>
 );
@@ -15,7 +16,7 @@ Index.getInitialProps = async function () {
   const data = await res.json();
 
   return {
-    data,
+    bpi: data.bpi,
   };
 };
 
